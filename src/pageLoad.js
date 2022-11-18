d1a085b2395cced3fba73e474779a6d353fd0c38
Joshua <joshuagpatten@gmail.com>
2022-11-18 14:04:25 -0500

    complete thru step 5

---
+++ src/pageLoad.js
@@ -0,0 +1,14 @@
+export const pageLoad = () => {
+  console.log("pageLoad test");
+  var header = document.createElement("H1");
+  header.innerText="Welcome to Borger.com";
+  var image = document.createElement("img");
+  image.src = "../borger.jpg";
+  image.alt = "Tasty looking borger";
+  var paragraph = document.createElement("p");
+  paragraph.innerText = "Look at this tasty borger. It has a vegan bun made from the finest Italian flour, a crisp piece of iceburg lettuce, a hearty home-grown tomato, spicy onions, authentic tomato puree, two slices of fake American cheese, and finally a juicy lean beef patty. Tasty huh? Too bad it isn't real."
+  const content = document.getElementById('content');
+  content.appendChild(header);
+  content.appendChild(image);
+  content.appendChild(paragraph);
+}
